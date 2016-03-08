a266dfa5e67fe14d8288d3be6d95a71e66d47198
TehGuy <iamtehguy@gmail.com>
2016-03-08 01:42:55 -0600

    Fixed your shit, Chance

--- API Functions.swift
+++ API Functions.swift
@@ -24,35 +24,9 @@ class API_Functions{
         var URL = "https://osu.ppy.sh/api/get_user?k=b842689a894e7c825998f2f3c490409bdd287af9&u="
         
         if(UserInfo.username.isEmpty){
-            let path = NSString(string: "~/Documents/Username.txt").stringByExpandingTildeInPath
-            let readFile = (try? NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)) as? String
-            if let fileContents = readFile{
-                output = fileContents as String
-            }
-            else{
-                print("Error reading file contents at " + path)
-                return false
-            }
-            
-            if (output.isEmpty){
-                return false
-            }
             output = readFromFile(output)
         }
         else{
-            let path = NSString(string: "~/Documents/Username.txt").stringByExpandingTildeInPath
-            var error = NSError(domain: "somedomain", code: 123, userInfo: nil)
-            let written:Bool
-            do{
-                try UserInfo.username.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding)
-                written = true
-            }
-            catch let error1 as NSError{
-                error = error1
-                written = false
-            }
-            if (!written) {
-                print("There was a problem: \(error)")
             if(!writeToFile()){
                 return false
             }
@@ -64,7 +38,7 @@ class API_Functions{
         
         let session = NSURLSession.sharedSession()
         let loginURL = NSURL(string: URL)!
-
+        
         let task = session.dataTaskWithURL(loginURL){(data, response, error) -> Void in
             let loginData = JSON(data: data!)
             
