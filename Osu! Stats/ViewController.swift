c2f9115992c505ffab8bb55cfff222e2f64bd1f6
TehGuy <iamtehguy@gmail.com>
2016-03-08 02:16:39 -0600

    each

--- Osu! Stats/ViewController.swift
+++ Osu! Stats/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     
     @IBAction func Login(sender: UIButton){
         UserInfo.setUsername(UsernameEntryBox.text!)
+        UsernameEntryBox.text = ""
     }
 }
 
