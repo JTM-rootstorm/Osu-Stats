68e6a873c34c398b10abffdf185966e0f934b9b0
lilchancep <lilchancep@gmail.com>
2016-03-08 02:15:50 -0600

    Fix whatever the fuck was making it crash

--- Osu! Stats/InfoViewController.swift
+++ Osu! Stats/InfoViewController.swift
@@ -12,8 +12,9 @@ class InfoViewController: UIViewController {
         
     @IBOutlet weak var lbl_username: UILabel!
     @IBOutlet weak var lbl_pprank: UILabel!
-
     @IBOutlet weak var lbl_level: UILabel!
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +51,7 @@ class InfoViewController: UIViewController {
             dispatch_async(dispatch_get_main_queue()){
                 self.lbl_username.text = UserInfo.username
                 self.lbl_pprank.text = UserInfo.pp_rank
-               // self.lbl_level.text = UserInfo.level
+                self.lbl_level.text = UserInfo.level
             }
         }
     }
